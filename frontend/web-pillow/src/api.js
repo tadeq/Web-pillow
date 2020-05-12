213725f3a047f67a703c0ec01d2648a17a1791fe
Maciej Moskal <mmtm1097@gmail.com>
2020-05-13 00:08:50 +0200

    initial commit

---
+++ frontend/web-pillow/src/api.js
@@ -0,0 +1,20 @@
+const host = "localhost";
+const port = 5444;
+const url = endpoint => `http://${host}:${port}/${endpoint}`;
+
+export default {
+    fetch: (opt, action) => {
+        return fetch(opt.path, {
+            method: opt.method,
+            body: opt.body,
+            headers: opt.headers,
+            credentials: 'include'
+        })
+            .then(res => res.json())
+            .then(response => {
+                action(response);
+            });
+    },
+    endpoints: {
+    }
+}
